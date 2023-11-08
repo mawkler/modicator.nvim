6fadbcbcff6dd582049aa1358f1e6f02b31d980c
Melker Ulander <melker.ulander@pm.me>
2023-11-08 16:34:03 +0100

    test: set mode highlights for default colorscheme

--- lua/modicator/tests/init.lua
+++ lua/modicator/tests/init.lua
@@ -4,8 +4,15 @@ local utils = require('modicator.utils')
 local highlight_exists = utils.highlight_exists
 local get_highlights = utils.get_highlights
 
+local function setup_modicator()
+  vim.o.termguicolors = true
+  vim.o.cursorline = true
+  vim.o.number = true
+  require('modicator').setup()
+end
+
 --- @param keys string
---- @param mode string
+--- @param mode string?
 local function feedkeys(keys, mode)
   if mode == nil then mode = 'n' end
 
@@ -25,17 +32,37 @@ local function sleep(ms)
 end
 
 describe('creates highlights', function()
-  it("has no modicator highlights before setup", function()
+  it('has no modicator highlights before setup', function()
     for _, highlight in pairs(get_highlights()) do
       assert.is_not_true(highlight_exists(highlight))
     end
   end)
 
   it('creates highlights on setup', function()
-    require('modicator').setup()
+    setup_modicator()
 
     for _, highlight in pairs(get_highlights()) do
       assert.is_true(highlight_exists(highlight))
     end
+  end)
+
+  it('sets mode highlights for default colorscheme', function()
+    local mode_hl_fgs = {}
+    for _, hl_name in pairs(utils.get_highlights()) do
+      mode_hl_fgs[hl_name] = modicator.get_highlight(hl_name).fg
+    end
+
+    local expected_fg_hls = {
+      CommandMode        = 16752800,
+      InsertMode         = 32768,
+      NormalMode         = 16776960,
+      ReplaceMode        = 16711935,
+      SelectMode         = 16777056,
+      TerminalMode       = 32768,
+      TerminalNormalMode = 16776960,
+      VisualMode         = 6356832,
+    }
+
+    assert.are.same(expected_fg_hls, mode_hl_fgs)
   end)
 end)
